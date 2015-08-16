62070aac259a9bf16a975226d9350b7b771b9455
Eystein Mack Alnæs <thatNorwegian@gmail.com>
2015-08-16 15:31:03 +0200

    Four JSON objects.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,98 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+* Four JSON objects
+*  education{}
+*  work{}
+*  projects{}
+*  bio{}
+*/
+
+
+// Object with array with two objects.
+var education = {
+  "schools" : [
+   {
+      "name" : "UiO",
+      "location" : "Oslo",
+      "degree" : "IT",
+      "majors" : ["nothing"],
+      "dates" : "1998-1999",
+      "url" : "http://uio.no"
+   },
+   {
+      "name" : "ECU",
+      "location" : "Perth",
+      "degree" : "Bachelor of Communications",
+      "majors" : ["Interactive Multimedia", "Film and Video"],
+      "dates" : "2001-2004",
+      "url" : "http://ecu.com.au"
+   }
+  ],
+  "onlineCourses" : [
+    {
+      "title" : "Smacss",
+      "school" : "Jonathan Snook",
+      "dates" : "July 2015",
+      "url" : "http://snook.ca"
+    },
+    {
+      "title" : "Javascript",
+      "school" : "Udacity",
+      "dates" : "August 2015",
+      "url" : "https://www.udacity.com"
+    }
+  ]
+}
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "selfemployed",
+      "title" : "Freelance web consultant",
+      "location" : ["Australia", "France", "Norway"],
+      "dates" : "2008 - 2015",
+      "description" : "Contracting front-end developer for medium and large firms"
+    },
+    {
+      "employer" : "Creuna",
+      "title" : "Front-end developer",
+      "location" : "Norway",
+      "dates" : "2007 - 2008",
+      "description" : "HTML, CSS and Mootools javascript library"
+    }
+  ]
+}
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "laks.no",
+      "dates" : "Aug-Nov 2015",
+      "description" : "Animated website with lottery app",
+      "images" : ["https://mikespennyforyourthoughts.files.wordpress.com/2011/03/mjm09_wireframe_2.jpg", "http://www.smartz.com/s/RRTgEbfGMEKV0KDYARmaNw/turn-wireframe.jpg"]
+    },
+    {
+      "title" : "eltekholding.no",
+      "dates" : "Jun-Aug 2015",
+      "description" : "One pager website with parallax and fullpage slides",
+      "images" : ["https://mikespennyforyourthoughts.files.wordpress.com/2011/03/mjm09_wireframe_2.jpg", "http://www.smartz.com/s/RRTgEbfGMEKV0KDYARmaNw/turn-wireframe.jpg"]
+    }
+  ]
+}
+
+ // Declares a object with key-value pairs
+var bio = {
+  "name" : "Eystein Alnaes",
+  "role" : "Web developer",
+  "contacts" : {
+    "mobile" : "+47 406 74 098",
+    "email" : "eystein@eystein.no",
+    "github" : "eystein",
+    "twitter" : "iceMagic",
+    "location" : "Chamonix"
+  },
+  "welcomeMessage" : "Caring about usablity",
+  "skills" : [
+    "CSS", "UX", "JavaScript", "CSS3 animation"
+  ],
+  "bioPic" : "https://pbs.twimg.com/profile_images/1794315645/L1040675_400x400.JPG"
+}
