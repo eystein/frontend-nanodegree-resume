ca910969fb7576d97d4c7948c0e403a02afdf27b
Eystein Mack Alnæs <thatNorwegian@gmail.com>
2015-08-24 13:46:41 +0200

    SVG bar chart

--- js/d3Builder.js
+++ js/d3Builder.js
@@ -26,23 +26,82 @@
 // section.append("div")
 //   .html("Second.");
 
-var data = [4, 8, 15, 16, 23, 42];
+/*
+*
+* 1. HTML based chart
+*     http://bost.ocks.org/mike/bar/
+*
+*/
+function HTMLcharterize() {
+  var data = [4, 8, 15, 16, 23, 42];
+
+  /*
+  * Scaling function to scale on a arbitrary
+  * width rather than in multiplies of 10.
+  */
+  var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 100]);
+
+
+  d3.select(".chartHTML")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) {
+        return x(d) + "px";
+      })
+      .text(function(d) {
+        return d;
+      })
+}
+
+HTMLcharterize();
+
 
 /*
-* Scaling function to scale on a arbitrary
-* width rather than in multiplies of 10.
+*
+* 2. SVG based chart
+*      http://bost.ocks.org/mike/bar/2/
+*
 */
-var x = d3.scale.linear()
-  .domain([0, d3.max(data)])
-  .range([0, 100]);
+function SVGcharterize() {
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) {
-      return x(d) + "px";
-    })
-    .text(function(d) {
-      return d;
-    })
+  var width = 420,
+      barHeight = 20;
+
+  var x = d3.scale.linear()
+    .range([0, width]);
+
+  var chart = d3.select(".chartSVG")
+    .attr("width", width);
+    // .attr("height", barHeight * data.length);
+
+  d3.tsv("js/data.tsv", type, function(error, data) {
+    x.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    chart.attr("height", barHeight * data.length);
+
+    var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .attr("height", barHeight - 1);
+
+    bar.append("text")
+      .attr("x", function(d) { return x(d.value) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", "0.35em")
+      .text(function(d) { return d.value ; });
+
+  });
+
+  function type(d) {
+    d.value = +d.value; // coerce to number
+    return d;
+  }
+}
+SVGcharterize();
