c3bfadf5cdc922f6182f7b67d0c43f5cfa28010e
Eystein Mack Alnæs <thatNorwegian@gmail.com>
2015-08-25 09:56:40 +0200

    Add code for proper chart.

--- js/d3Builder.js
+++ js/d3Builder.js
@@ -105,3 +105,55 @@ function SVGcharterize() {
   }
 }
 SVGcharterize();
+
+function fullChart() {
+  var width = 960,
+      height = 500;
+
+  var y = d3.scale.linear()
+          .range([height, 0]);
+
+  var chart = d3.select(".chartFull")
+              .attr("width", width)
+              .attr("height", height);
+
+  d3.tsv("js/data.tsv", type, function(error, data) {
+    y.domain([0, d3.max(data, function(d) {
+      return d.value;
+    })]);
+
+    var barWidth = width / data.length;
+
+    var bar = chart.selectAll("g")
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) {
+        return "translate(" + i * barWidth _ ",0)";
+      });
+
+    bar.append("rect")
+      .attr("y", function(d) {
+        return y(d.value);
+      })
+      .attr("height", function(d) {
+        return height - y(d.value);
+      })
+      .attr("width", barWidth - 1);
+
+    bar.append("text")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) {
+        return y(d.value) + 3;
+      })
+      .attr("dy", ".75em")
+      .text(function(d) {
+        return d.value;
+      })
+
+  });
+
+  function type(d) {
+    d.value = +d.value;
+    return d;
+  }
+}
