07e8cfca1b7da04661d096ea912a2b81dc7e5532
Eystein Mack Alnæs <thatNorwegian@gmail.com>
2015-08-28 15:00:47 +0200

    Add marker info bubbles on map.

--- js/helper.js
+++ js/helper.js
@@ -173,6 +173,8 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      console.log('click ' + name);
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
